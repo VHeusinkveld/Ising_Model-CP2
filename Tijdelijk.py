915dd2ba553e04835a7ebd28d3e2267da722d324
Tom de Krom <tom.de.krom@gmail.com>
2018-05-05 22:24:59 +0200

    Removal of redundant files

--- Tijdelijk.py
+++
@@ -1,9 +0,0 @@
-def f_chi(tau, factor, a, const):
-    return factor*abs(tau)**(-a) + const
-
-def f_cv(tau, factor, a, const):
-    return factor*abs(tau)**(-a) + const
-# in which tau = T-T_c
-
-def f_magnetisation(tau, factor, beta, const):
-    return factor*(-1*tau)**(beta) + const
