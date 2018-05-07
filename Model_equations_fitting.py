5a887a31bed583031fd8f8ab82e8d2d599865d6d
Tom de Krom <tom.de.krom@gmail.com>
2018-05-07 15:12:53 +0200

    Updated modelling functions and new figure of fit for chi with 5000 MC

--- Model_equations_fitting.py
+++ Model_equations_fitting.py
@@ -7,4 +7,4 @@ def f_cv(tau, factor):
     return factor*np.log(tau)
 
 def f_magnetisation(tau, factor, a):
-    return factor*tau**(a) #(No minus at tau, as there is already taken care of in fitting function)
+    return factor*tau**a #(No minus at tau, as there is already taken care of in fitting function)
